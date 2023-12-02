88a107316ef6fc3553b3ad4ab51353bd76be5f71
ishtiaqSamdani <ishmartishtiaq709@gmail.com>
2023-12-02 19:21:04 +0530

    refactor: chnage workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // Ensure correct base path for GitHub Pages
   plugins: [react()],
-})
+  build: {
+    outDir: "build", // Specify the output directory
+    assetsDir: ".", // Specify the assets directory
+  },
+});
